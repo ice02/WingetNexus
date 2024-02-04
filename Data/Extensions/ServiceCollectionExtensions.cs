71a929b2d6c7feb9f62bd9634c0568f44532f401
ice02 <{ID}+{username}@users.noreply.github.com>
2024-02-04 19:11:21 +0100

    adding redis l2 cache

--- Data/Extensions/ServiceCollectionExtensions.cs
+++ Data/Extensions/ServiceCollectionExtensions.cs
@@ -1,11 +1,9 @@
 ﻿using EFCoreSecondLevelCacheInterceptor;
+using MessagePack;
+using MessagePack.Formatters;
+using MessagePack.Resolvers;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WingetNexus.Data.Extensions
 {
@@ -114,17 +112,84 @@ namespace WingetNexus.Data.Extensions
                     .AddInterceptors(serviceProvider.GetRequiredService<SecondLevelCacheInterceptor>());
             });
 
-            services.AddEFSecondLevelCache(options =>
+            //services.AddEFSecondLevelCache(options =>
+            //{
+            //    if (allowCache)
+            //    {
+            //        options.UseMemoryCacheProvider().DisableLogging(true);
+            //        options.CacheAllQueries(CacheExpirationMode.Sliding, TimeSpan.FromMinutes(30));
+            //    }
+            //});
+
+            
+            if (allowCache)
             {
-                if (allowCache)
+                const string providerName1 = "Redis1";
+
+                services.AddEFSecondLevelCache(options =>
+                        options.UseEasyCachingCoreProvider(providerName1, isHybridCache: false)
+                        .DisableLogging(true)
+                        .UseCacheKeyPrefix("EF_")
+                        // Fallback on db if the caching provider fails (for example, if Redis is down).
+                        .UseDbCallsIfCachingProviderIsDown(TimeSpan.FromMinutes(1))
+                );
+
+
+                // More info: https://easycaching.readthedocs.io/en/latest/Redis/
+                services.AddEasyCaching(option =>
                 {
-                    options.UseMemoryCacheProvider().DisableLogging(true);
-                    options.CacheAllQueries(CacheExpirationMode.Sliding, TimeSpan.FromMinutes(30));
-                }
-            });
+                    option.UseRedis(config =>
+                    {
+                        config.DBConfig.AllowAdmin = true;
+                        config.DBConfig.SyncTimeout = 10000;
+                        config.DBConfig.AsyncTimeout = 10000;
+                        config.DBConfig.Endpoints.Add(new EasyCaching.Core.Configurations.ServerEndPoint("127.0.0.1", 6379));
+                        config.EnableLogging = true;
+                        config.SerializerName = "Pack";
+                        config.DBConfig.ConnectionTimeout = 10000;
+                    }, providerName1)
+                    .WithMessagePack(so =>
+                    {
+                        so.EnableCustomResolver = true;
+                        so.CustomResolvers = CompositeResolver.Create(
+                            new IMessagePackFormatter[]
+                            {
+                            DbNullFormatter.Instance, // This is necessary for the null values
+                            },
+                            new IFormatterResolver[]
+                            {
+                            NativeDateTimeResolver.Instance,
+                            ContractlessStandardResolver.Instance,
+                            StandardResolverAllowPrivate.Instance,
+                            }
+                        );
+                    },
+                    "Pack");
+                });
+            }
 
             return services;
         }
 
+    }
+
+    public class DbNullFormatter : IMessagePackFormatter<DBNull>
+    {
+        public static DbNullFormatter Instance = new();
+
+        private DbNullFormatter()
+        {
+        }
+
+        public void Serialize(ref MessagePackWriter writer, DBNull value, MessagePackSerializerOptions options)
+        {
+            // always serialize as nil (if present, it's never null)
+            writer.WriteNil();
+        }
+
+        public DBNull Deserialize(ref MessagePackReader reader, MessagePackSerializerOptions options)
+        {
+            return DBNull.Value;
+        }
     }
 }
